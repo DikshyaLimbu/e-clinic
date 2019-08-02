1968591d7869d36c3c557a2eb5c6e60e070e9edb
Bimal Raj Gyawali <gyawali.rajbimal35@gmail.com>
2019-08-02 17:07:18 +0545

    Add files via upload
    demo change

---
+++ Major Project/Captcha/captcha.php
@@ -0,0 +1,4 @@
+session_start();
+$captcha_num = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz';
+$captcha_num = substr(str_shuffle($captcha_num), 0,6);
+$_SESSION["code"] = $captcha_num;
